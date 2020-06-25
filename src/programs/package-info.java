1933b4c4b97716a3fee3c0068e5b4721562f2534
Prashant Singh Chauhan <chauhanprashant480@gmail.com>
2020-06-25 07:43:20 +0530

    junit jupiter based test class added for StringCalculator

---
+++ src/programs/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author heyau
+ *
+ */
+package programs;
---
+++ src/tests/TestStringCalculator.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author heyau
+ *
+ */
+class TestStringCalculator {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
