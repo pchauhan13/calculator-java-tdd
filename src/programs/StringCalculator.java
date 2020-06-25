4a66d0402bf236c4d9be0476d841cba60d255fa4
Prashant Singh Chauhan <chauhanprashant480@gmail.com>
2020-06-25 10:43:22 +0530

    add method updated to handle more than one delimiter and make the test case green

--- src/programs/StringCalculator.java
+++ src/programs/StringCalculator.java
@@ -13,19 +13,25 @@ public class StringCalculator {
 		int sum = 0;
 		String negs = "";
 		String delimiter = ",|\n";
+		String anotherDelimiter = "";
 		if (string.isEmpty()) {
 			return sum;
 		}
 		
 		if (string.startsWith("//[")) {
 			delimiter = (String) string.subSequence(string.indexOf("[") + 1, string.indexOf("]"));
-			string = string.substring(string.indexOf("\n") + 1);
 			delimiter = delimiter.replaceAll("\\*", "\\\\*");
+			if (string.indexOf("[") != string.lastIndexOf("[")) {
+				anotherDelimiter = (String) string.subSequence(string.lastIndexOf("[") + 1, string.lastIndexOf("]"));
+				anotherDelimiter = anotherDelimiter.replaceAll("\\*", "\\\\*");
+				delimiter = delimiter.concat("|" + anotherDelimiter);
+			}
+			string = string.substring(string.indexOf("\n") + 1);
 		} else if (string.startsWith("//")) {
-			delimiter = Character.toString(string.charAt(2));
+			delimiter = Character.toString(string.charAt(2)).replaceAll("\\*", "\\\\*");
 			string = string.substring(4);
 		}
-
+		
 		for (String str : string.split(delimiter)) {
 			int num = Integer.parseInt(str);
 			if (num < 0 && negs.isEmpty()) {
